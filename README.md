11af16157e68a4bffe1a320e3c763fdfde67a6d3
xnought <bertuccd@oregonstate.edu>
2022-11-02 21:18:28 -0700

    qr decomp in matlab

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# matrix-factorizations
+# Matrix Factorizations
+
 Matrix factorizations/decompositions in random languages I'm feeling
+
+I'll venture to other languages when I feel like it.
+
+Right now, I'm doing matlab because its required for a class in numerical linear algebra.
---
+++ matlab/qr.m
@@ -0,0 +1,31 @@
+% QR DECOMPOSITION
+% running time: O(2n^2(m-n/3)) ~ O(mn^2)
+function [Q, R] = their_qr(A)
+    [m, n] = size(A);
+    Q = eye(m);
+
+    % iterate over each column going down a pivot
+    for i = 1:n
+        % column starting from pivot
+        x = A(i:m, i);
+
+        e_i = eye(m - i + 1, 1); % canonical unit vector [1, 0, ..., 0]^T
+        v = x + sign(x) .* norm(x, 2) .* e_i % x +- ||x||e_i
+        v = v / sqrt(v' * v); % norm to 1
+
+        % House holder reflections
+        for j = 1:n
+            x = A(i:n, j);
+            A(i:n, j) = x - (2 * v * v' * x);
+        end
+
+        for j = 1:m
+            x = Q(i:m, j);
+            Q(i:m, j) = x - (2 * v * v' * x);
+        end
+
+    end
+
+    Q = Q'; % when Q^-1 = Q^* and so forth
+    R = triu(A); % take the upper triangular entries
+end
